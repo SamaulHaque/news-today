e77a06fa26ea3cb8e1996a7fa88b68bc8cbb62be
SamaulHaque <samaulhaque749@gmail.com>
2022-09-03 20:17:51 +0600

    category news sort by total_view

--- news.js
+++ news.js
@@ -1,18 +1,22 @@
+//all news category load
 const newsLoad=async()=>{
     const url=`https://openapi.programming-hero.com/api/news/categories`;
-    const res=await fetch(url);
-    const data=await res.json();
-    displayNews(data.data.news_category);
+    //error handler
+    try{
+        const res=await fetch(url);
+        const data=await res.json();
+        displayNews(data.data.news_category);
+    }
+    catch (error){
+        console.log(error);
+    }
 }
 
+//all news category display
 const displayNews=(newses)=>{
     const newsContainer=document.getElementById('news-container');
-    
-    
-
     newses.forEach(news=>{
         const newsDiv=document.createElement('div');
-        newsDiv.classList.add('display');
         newsDiv.innerHTML=`
         <div>
         <a href="#" onclick="newsCategoryDetails('${news.category_id}')" class="category"> ${news.category_name}</a>
@@ -21,32 +25,38 @@ const displayNews=(newses)=>{
         newsContainer.appendChild(newsDiv);
     })
 }
+newsLoad();//all news category function call
 
-
-newsLoad();
-
-
+//all news details in a category load
 const newsCategoryDetails=async id=>{
-
     loader(true);//spiner loading start
-
     const url=`https://openapi.programming-hero.com/api/news/category/${id}`;
-    const res=await fetch(url);
-    const data=await res.json();
-    displayCategoryDetails(data.data);
+    //error handler
+    try{
+        const res=await fetch(url);
+        const data=await res.json();
+        displayCategoryDetails(data.data);
+    }
+    catch(error){
+        console.log(error);
+    }
     
 }
 
+//all news details in a category display
 const displayCategoryDetails=(details)=>{
-    console.log(details)
-    details.sort((a,b)=>{
-        return b.propertyName-a.propertyName
-    });
+    
+    //sort by total-view max value
+    const sorting=(a,b)=>{
+        return b.total_view - a.total_view;
+    }
+    details.sort(sorting);
 
     const categoryContainer=document.getElementById('category-container');
     categoryContainer.innerText='';
 
     details.forEach(detail=>{
+        console.log(detail);
         const categoryDiv=document.createElement('div');
         categoryDiv.classList.add('row');
         categoryDiv.innerHTML=`
@@ -71,21 +81,19 @@ const displayCategoryDetails=(details)=>{
             </div>
         `;
         categoryContainer.appendChild(categoryDiv);
-        
     })
     loader(false);//spiner loading end
-
-    
 
     //news items count
     const itemsCountField=document.getElementById('items-count-field');
     const itemsCuantity=details.length;
     const defaultText='Items Found For This Category'
     itemsCountField.value=`${itemsCuantity} ${defaultText}`;
-    
 }
 
 
+
+//spiner loading
 const loader=isLoading=>{
     const loader=document.getElementById('loader')
     if(isLoading){
@@ -96,40 +104,45 @@ const loader=isLoading=>{
     }
 }
 
-
+//display news details in a modal 
 const loadNewsDetails=async _id=>{
     const url=`https://openapi.programming-hero.com/api/news/${_id}`;
-    const res=await fetch(url);
-    const data=await res.json();
-    displayNewsDetails(data.data);
+    //error handler
+    try{
+        const res=await fetch(url);
+        const data=await res.json();
+        displayNewsDetails(data.data);
+    }
+    catch(error){
+        console.log(error)
+    }
 }
 
-
+//display news details in a modal display
 const displayNewsDetails=(detail)=>{
-        
+
         detail.forEach(detailNews=>{
             const newsTitle=document.getElementById('newsDetailsModalLabel')
             newsTitle.innerText=detailNews.title ? detailNews.title : 'Title not found';
 
-            console.log(detailNews);
             const modalBodyDiv=document.getElementById('news-details-body');
             modalBodyDiv.innerHTML=`
-                <p>Category Id: ${detailNews.category_id ? detailNews.category_id: 'Category Id Not Found'}</p>
+              <p>Category Id: ${detailNews.category_id ? detailNews.category_id: 'Category Id Not Found'}</p>
 
-                <p>Author Name: ${detailNews.author.name ? detailNews.author.name: 'Author Name Not Found'}</p>
+              <p>Author Name: ${detailNews.author.name ? detailNews.author.name: 'Author Name Not Found'}</p>
 
-                <p>Publish Date: ${detailNews.author.published_date ? detailNews.author.published_date: 'Publish Not Found'}</p>
+              <p>Publish Date: ${detailNews.author.published_date ? detailNews.author.published_date: 'Publish Not Found'}</p>
 
-                <img src="${detailNews.thumbnail_url}">
+              <img src="${detailNews.thumbnail_url}">
 
-                <p>Details: ${detailNews.details ? detailNews.details: 'Details Not Found'}</p>
+              <p>Details: ${detailNews.details ? detailNews.details: 'Details Not Found'}</p>
 
-                <p>Number: ${detailNews.rating.number ? detailNews.rating.number: 'Number Not Found'}</p>
+              <p>Number: ${detailNews.rating.number ? detailNews.rating.number: 'Number Not Found'}</p>
 
-                <p>Badge: ${detailNews.rating.badge ? detailNews.rating.badge: 'Badge Not Found'}</p>
+              <p>Badge: ${detailNews.rating.badge ? detailNews.rating.badge: 'Badge Not Found'}</p>
 
-                <p>Total View: ${detailNews.total_view ? detailNews.total_view: 'Total View Not Found'}</p>
-            `
+              <p>Total View: ${detailNews.total_view ? detailNews.total_view: 'Total View Not Found'}</p>
+            `;
         })
 
 }
